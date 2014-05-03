362a2eba85105636d68297188daf6fa280483a67
cfbrazzolini <cfbrazzolini@gmail.com>
2014-05-03 17:57:32 -0300

    TR1 Completo

---
+++ build/CMakeFiles/src.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Caina/GitHub/game/src/Game.cpp" "C:/Users/Caina/GitHub/game/build/CMakeFiles/src.dir/Game.cpp.obj"
+  "C:/Users/Caina/GitHub/game/src/GameBase.cpp" "C:/Users/Caina/GitHub/game/build/CMakeFiles/src.dir/GameBase.cpp.obj"
+  "C:/Users/Caina/GitHub/game/src/Sprite.cpp" "C:/Users/Caina/GitHub/game/build/CMakeFiles/src.dir/Sprite.cpp.obj"
+  "C:/Users/Caina/GitHub/game/src/main.cpp" "C:/Users/Caina/GitHub/game/build/CMakeFiles/src.dir/main.cpp.obj"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "C:/SDL-2.0.3/include/SDL2"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ build/CMakeFiles/src.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/src.dir/Game.cpp.obj"
+  "CMakeFiles/src.dir/GameBase.cpp.obj"
+  "CMakeFiles/src.dir/main.cpp.obj"
+  "CMakeFiles/src.dir/Sprite.cpp.obj"
+  "src.pdb"
+  "src.exe"
+  "src.exe.manifest"
+  "libsrc.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/src.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
